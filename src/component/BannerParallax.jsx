4e69d33d0a3e2f44366e17c08ba6b27a609dbf49
Florian Toribio <58748740+sepios87@users.noreply.github.com>
2020-11-04 14:12:17 +0100

    v4

--- src/component/BannerParallax.jsx
+++ src/component/BannerParallax.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
 import ParallaxCache from './ParallaxCache'
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default function BannerParallax(props){
+
+	const matches = useMediaQuery('(min-width:992px)');
 
     return (
             <ParallaxProvider>
 				<ParallaxCache/>
-				<ParallaxBanner
+				<ParallaxBanner 
+					disabled = {matches ? false : true}
 					layers={[{image: props.lien, amount: 0.6, expanded : false}]}
-					style={{height: '320px', margin : "180px 0"}}
+					style={{height: matches ? '320px' : "180px", margin : matches ? "180px 0" : "80px 0"}}
 				/>
 			</ParallaxProvider>
     );
